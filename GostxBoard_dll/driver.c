2a9e7498577176bcfd3918965eebd5c37737b46c
WhiteKernel <amicelli@whitekernel.fr>
2015-10-02 15:11:27 +0200

    Updoate license

--- GostxBoard_dll/driver.c
+++ GostxBoard_dll/driver.c
@@ -2,27 +2,26 @@
 // 					This file is part of GostxBoard						//
 //																		//
 //  GostxBoard is free software: you can redistribute it and/or modify	//
-//       it under the terms of the Creative Commons [Attribution-		//	
-//	NonCommercial-ShareAlike 4.0 International] License as published	// 
-//				 by the Ditribution of Creative Commons 				//		
+//   it under the terms of the GNU General Public License as published 	//
+// 	 by the Free Software Foundation, either version 3 of the licence,  //
+//						  or any later version.							//	
 //                Such things are STRONGLY recommended.					//
 //    																	//
-// 	GostxBoard is distributed in the hope that it will be useful,		//
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of		//	
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		//
-//  	Creative Commons [BY-NC-SA] License for more details.			//	
+// 	  GostxBoard is distributed in the hope that it will be useful,		//
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of		//	
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		//
+//  	      GNU General Public License for more details.		    	//	
 //  Do not blame the developper if something goes wrong. You can 		//	
 //  either search on the Internet or email the developper to deal 		//
 //                             with it.									//
 //																		//
-//  You should have received a copy of the Creative Commons [BY-NC-SA]	// 
-//	             License along with GostxBoard. If not, see 			//
-//	  <https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode>. 	//
+//    You should have received a copy of GNU General Public License   	// 
+//	                along with GostxBoard. If not, see 		    	    //
+//	           <http://www.gnu.org/licenses/gpl-3.0.fr.html>.       	//
 //																		//
 //                              P.A 									//
 //                gostxboard_support@whitekernel.fr 					//
 //**********************************************************************//
-
 
 /**
 * \~English
